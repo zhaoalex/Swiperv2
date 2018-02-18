13c945c7b8daf4da47a0564676e9ed04427b7789
ysong <yuanping.song@outlook.com>
2018-02-17 17:44:55 -0800

    View Controller Commit

--- swiperv2/ViewController.swift
+++ swiperv2/ViewController.swift
@@ -9,7 +9,13 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var options : [String] = ["Bruin Plate", "Covel Commons", "De Neve", "Feast at Rieber", "Bruin Café", "1919", "The Study at Hedrick"]
+    @IBOutlet weak var resultLabel: UILabel!
+    @IBAction func makeDecision(_ sender: UIButton) {
+        let idx = Int(arc4random_uniform(7))
+        resultLabel.text = "You should eat at " + options[idx]
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
