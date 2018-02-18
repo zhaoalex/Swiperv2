3e8470a592f298da4f24ae44006c991df470b9d4
Alex Zhao <alexzhao21@gmail.com>
2018-02-17 18:37:48 -0800

    Get live data

--- swiperv2/ViewController.swift
+++ swiperv2/ViewController.swift
@@ -7,17 +7,19 @@
 //
 
 import UIKit
+import SwiftSoup
 
 class ViewController: UIViewController {
-    var options : [String] = ["Bruin Plate", "Covel Commons", "De Neve", "Feast at Rieber", "Bruin Café", "1919", "The Study at Hedrick"]
+    var options: [String]?
     @IBOutlet weak var resultLabel: UILabel!
     @IBAction func makeDecision(_ sender: UIButton) {
-        let idx = Int(arc4random_uniform(7))
-        resultLabel.text = "You should eat at " + options[idx]
+        let idx = Int(arc4random_uniform(UInt32(options!.count)))
+        resultLabel.text = "You should eat at " + options![idx]
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        options = getData()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -25,7 +27,41 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func getData() -> Array<String> {
+        let myURLString = "http://menu.dining.ucla.edu"
+        guard let myURL = URL(string: myURLString) else {
+            print("Error: \(myURLString) doesn't seem to be a valid URL")
+            return [String]()
+        }
+        
+        do {
+            let myHTMLString = try String(contentsOf: myURL, encoding: .ascii)
+            //print(myHTMLString)
+            let doc: Document = try! SwiftSoup.parse(myHTMLString)
+            let mainContent: Elements = try! doc.select("#main-content").select("div").select("h2 ~ div").select(".half-col,.whole-col")
+            
+            var food = [String]()
+            
+            for div: Element in mainContent.array() {
+                let p: Elements = try! div.select(".unit-name")
+                for thing: Element in p.array() {
+                    let name: String = try! thing.text()
+                    food.append(name)
+                }
+            }
+            
+            /*for div in mainContent {
+             print(div)
+             
+             }*/
+            
+            return food
+        } catch let error {
+            print("Error: \(error)")
+        }
+        return [String]()
+    }
 
 
 }
-
